bfca4dea7b8af3625a6366189b62f7cb66be9316
Andrei <amititelu95@gmail.com>
2022-10-02 15:22:05 +0300

    Am adaugat explicatiile aferente codului

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-document.querySelector(".calculate").addEventListener("click", function () {
-  document.getElementById("value").style.display = "none";
+//Adaugam EventListener pentru a putea activa butonul de Calculate 
 
+  document.querySelector(".calculate").addEventListener("click", function () {
+  
+    //Hide output
+  document.getElementById("value").style.display = "none";
+  
+  //Afisam loaderul
   document.querySelector(".loading").style.display = "block";
 
   setTimeout(BMI, 2000);
 });
+
+//Clear EventListener
 document.querySelector(".clear").addEventListener("click", Clear);
 
+// BMI Calculation Function
 function BMI() {
   const height = document.getElementById("height").value;
   const weight = document.getElementById("weight").value;
-
+  // Formula de calcul
   let index = weight / (((height / 100) * height) / 100).toFixed(0);
 
   const value = document.getElementById("value");
   const explanation = document.getElementById("explanation");
   value.innerHTML = "Your BMI is: " + index;
+
+  //Ascundem Output-ul si Loader-ul timp de 2 secunde
   document.getElementById("value").style.display = "block";
   document.querySelector(".loading").style.display = "none";
 
@@ -26,6 +36,7 @@ function BMI() {
 
     // Cu switch-ul vom returna un singur key
     // Din map o sa citim key-ul
+    // Creez o conditie care sa verifice daca inaltimea si greutatea nu sunt egale cu 0
     switch (height && weight != 0) {
       case index < 15:
         status = "extreme_underweight";
@@ -56,22 +67,30 @@ function BMI() {
         error();
 
         function error() {
+          //Ascundem Output-ul si Loader-ul
           document.getElementById("value").style.display = "block";
           document.querySelector(".loading").style.display = "none";
+          
+          //Afisam mesajul de eroare
           document.getElementById("value").innerHTML =
             "Please enter a valid height and weight!";
-          setTimeout(() => {
+          
+          //Afisam mesajul de eroare pentru 5 secunde
+            setTimeout(() => {
             document.getElementById("value").innerHTML = "";
             document.querySelector(".loading").innerHTML = "";
           }, 5000);
         }
     }
 
+    // Returnam key-ul
     return status;
   };
 
+  //Variabila currentStatus va lua valoarea functiei calculateStatus
   const currentStatus = calculateStatus(index);
 
+  //Map pentru a putea afisa mesajul corespunzator BMI-ului
   const mappedInformation = {
     extreme_underweight: {
       color: "red",
@@ -147,6 +166,8 @@ function BMI() {
   localStorage.setItem("application-bmi", JSON.stringify(dataToSave));
   }
 }
+
+//Functia de Clear pentru Output si State
 
 function Clear() {
   document.getElementById("height").value = "";
